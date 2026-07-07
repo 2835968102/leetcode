0f6d33900c213e7a05033bcb5e1c7910d79fcf61
pml <2835968102@qq.com>
2026-07-07 09:48:38 +0800

    feat: add delete and earn solution

---
+++ src/dp/_740_DeleteAndEarn.java
@@ -0,0 +1,44 @@
+package dp;
+
+/**
+ * 740. Delete and Earn
+ * https://leetcode.cn/problems/delete-and-earn/
+ *
+ * 思路：
+ * 先把每个数字 x 的总贡献累加到 sum[x]。
+ * 之后问题转化为打家劫舍：选择 i 就不能选择 i - 1 和 i + 1。
+ *
+ * 时间复杂度：O(n + max(nums))
+ * 空间复杂度：O(max(nums))
+ */
+public class _740_DeleteAndEarn {
+    public int deleteAndEarn(int[] nums) {
+        int max = 0;
+        for (int x : nums) {
+            max = Math.max(max, x);
+        }
+
+        int[] sum = new int[max + 1];
+
+        for (int x : nums) {
+            sum[x] += x;
+        }
+
+        if (max == 0) {
+            return 0;
+        }
+        if (max == 1) {
+            return sum[1];
+        }
+
+        int[] dp = new int[max + 1];
+        dp[0] = sum[0];
+        dp[1] = Math.max(sum[0], sum[1]);
+
+        for (int i = 2; i <= max; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + sum[i]);
+        }
+
+        return dp[max];
+    }
+}
